224ea55bf5daed5189d3da0b5329ff8209b51d4e
Morrism1 <morrismontana0@gmail.com>
2021-01-07 02:16:00 +0200

    Refactor the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,40 @@
 import React from 'react'
 
+const today = new Date()
+
 const Footer = () => (
   <div id="footer">
     <div className="inner">
       <ul className="icons">
         <li>
-          <a href="#" className="icon fa-twitter">
+          <a
+            href="https://twitter.com/MurangwaMorris/"
+            className="icon fa-twitter"
+          >
             <span className="label">Twitter</span>
           </a>
         </li>
         <li>
-          <a href="#" className="icon fa-github">
+          <a href="https://github.com/Morrism1" className="icon fa-github">
             <span className="label">Github</span>
           </a>
         </li>
         <li>
-          <a href="#" className="icon fa-dribbble">
-            <span className="label">Dribbble</span>
+          <a
+            href="https://www.linkedin.com/in/mauricemurangwa/"
+            className="icon fa-linkedin"
+          >
+            <span className="label">Linked_in</span>
           </a>
         </li>
         <li>
-          <a href="#" className="icon fa-envelope-o">
+          <a href="mailto:maurice@octan.group" className="icon fa-envelope-o">
             <span className="label">Email</span>
           </a>
         </li>
       </ul>
       <ul className="copyright">
-        <li>&copy; Gatsby Starter Strata</li>
-        <li>
-          Design: <a href="http://html5up.net">HTML5 UP</a>
-        </li>
+        <li>&copy; Maurice Murangwa - {today.getFullYear()}</li>
       </ul>
     </div>
   </div>
