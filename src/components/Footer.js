586d0fbc220496115b43e02e0be4bf0c1d741447
Morrism1 <morrismontana0@gmail.com>
2021-03-29 22:07:28 +0200

    Add target to links and Angelist link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,12 +10,13 @@ const Footer = () => (
           <a
             href="https://twitter.com/MurangwaMorris/"
             className="icon fa-twitter"
+            target="_blank"
           >
             <span className="label">Twitter</span>
           </a>
         </li>
         <li>
-          <a href="https://github.com/Morrism1" className="icon fa-github">
+          <a href="https://github.com/Morrism1" className="icon fa-github" target="_blank">
             <span className="label">Github</span>
           </a>
         </li>
@@ -23,12 +24,22 @@ const Footer = () => (
           <a
             href="https://www.linkedin.com/in/mauricemurangwa/"
             className="icon fa-linkedin"
+            target="_blank"
           >
             <span className="label">Linked_in</span>
           </a>
         </li>
         <li>
-          <a href="mailto:maurice@octan.group" className="icon fa-envelope-o">
+          <a
+            href="https://angel.co/u/maurice-murangwa"
+            className="icon fa-angellist"
+            target="_blank"
+          >
+            <span className="label">Linked_in</span>
+          </a>
+        </li>
+        <li>
+          <a href="mailto:maurice@octan.group" className="icon fa-envelope-o" target="_blank">
             <span className="label">Email</span>
           </a>
         </li>
